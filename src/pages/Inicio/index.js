efd05db71022b2c629c2c6be81cadcfd508dfb78
Ezequiel Brilhante <e.ribeiro.brilhante@accenture.com>
2022-12-19 19:50:33 -0300

    Criação do componente Titulo e Rodape

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,7 +1,19 @@
+import Banner from 'components/Banner';
+import Rodape from 'components/Rodape';
+import Titulo from 'components/Titulo';
 import Cabecalho from './../../components/Cabecalho/index';
 
 function Inicio() {
-  return <Cabecalho />;
+  return (
+    <>
+      <Cabecalho />
+      <Banner imagem="home" />
+      <Titulo>
+        <h1>Um lugar para guardar seus vídeos e filmes</h1>
+      </Titulo>
+      <Rodape />
+    </>
+  );
 }
 
 export default Inicio;
